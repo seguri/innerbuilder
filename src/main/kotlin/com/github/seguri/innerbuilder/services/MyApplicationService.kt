2695f854b0ef903208c132ab5c64857b2887887d
GitHub Action <action@github.com>
2020-12-10 22:02:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/seguri/innerbuilder/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/seguri/innerbuilder/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.seguri.innerbuilder.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.seguri.innerbuilder.MyBundle
 
 class MyApplicationService {
 
